b5ea9be8b6183c1fed48c06577c99e6e9af3fe84
= <oaemdl@gmail.com>
2023-11-25 17:39:13 -0500

    cs

--- ui/frmLogin.cs
+++ ui/frmLogin.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows.Forms;
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
 
 namespace appPlanilla_CS_4DS.ui
 {
@@ -9,9 +11,52 @@ namespace appPlanilla_CS_4DS.ui
             InitializeComponent();
         }
 
-        private void frmLogin_Load(object sender, System.EventArgs e)
+        private void frmLogin_Load(object sender, EventArgs e)
         {
-      
+
+        }
+
+        private void btn_Enter(object sender, EventArgs e)
+        {
+            ((Button)sender).BackColor = Color.Black;
+        }
+
+        private void btn_Leave(object sender, EventArgs e)
+        {
+            ((Button)sender).BackColor = Color.FromArgb(94,17,90);
+        }
+
+        private void txt_Enter(object sender, EventArgs e)
+        {
+            ((TextBox)sender).BackColor = Color.Aquamarine;
+        }
+
+        private void txt_Leave(object sender, EventArgs e)
+        {
+            ((TextBox)sender).BackColor = Color.White;
+        }
+
+        private void txt_KeyPress(object sender, KeyPressEventArgs e)
+        {
+
+        }
+
+        private void btnIniciar_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnCancelar_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pibSalir_Click(object sender, EventArgs e)
+        {
+            if ( MessageBox.Show("¿Desea salir?", "Salir", 
+                 MessageBoxButtons.YesNo, 
+                 MessageBoxIcon.Question ) == DialogResult.Yes )
+                Application.Exit();
         }
     }
 }
